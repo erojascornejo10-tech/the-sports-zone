7eed21e26146384dd16be54ebbade55d0b2b2823
EDUARDO ROJAS CORNEJO <EROJASCORNEJO10@MAIL.WOU.EDU>
2025-12-15 10:40:55 -0800

    my-first-web-page

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+alert('Welcome to my page');
+
+let userName = prompt('What is your name?');
+console.log(userName);
+
+
+//string concatenation
+document.write('Welcome' + userName + '!');

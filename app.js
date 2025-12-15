617882c52d5d872815e7fe0a7d2fb867cd3a5f49
EDUARDO ROJAS CORNEJO <EROJASCORNEJO10@MAIL.WOU.EDU>
2025-12-15 14:12:47 -0800

    the-boxing zone

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ alert('Welcome to my page');
 let userName = prompt('What is your name?');
 console.log(userName);
 
+let answer = confirm("Are you sure you want to continue to this page?");
+
+if (answer) {
+  console.log("Action confirmed");
+} else {
+  console.log("Action canceled");
+}
 
 //string concatenation
 document.write('Welcome' + userName + '!');
